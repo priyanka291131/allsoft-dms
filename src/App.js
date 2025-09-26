e452624ca1ed43fba28dbef5704f7789ae0cabd8
Priyanka tripathy <137667343+priyanka291131@users.noreply.github.com>
2025-09-26 14:35:07 +0530

    route App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginRegisterPage from "./pages/LoginRegisterPage";
+import DashboardPage from "./pages/DashboardPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginRegisterPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+    </Routes>
   );
-}
+};
 
 export default App;
